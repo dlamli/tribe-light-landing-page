7a16bc578ef76d85a6c52d467ff4d5aaa78d1604
dlamli <dannylamli@hotmail.com>
2024-11-13 22:24:15 -0600

    feat:Update LogoTicker Animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -40,38 +40,40 @@ const logos = [
 export const LogoTicker = () => {
   return (
     <div className="py-8 md:py-12 bg-white">
-      <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-        <motion.div
-          className="flex gap-14 flex-none pr-14"
-          animate={{
-            translateX: "-50%",
-          }}
-          transition={{
-            duration: 10,
-            repeat: Infinity,
-            ease: "linear",
-            repeatType: "loop",
-          }}
-        >
-          {/* First logo set for animation */}
-          {logos.map((logo) => (
-            <Image
-              key={logo.alt}
-              src={logo.src}
-              alt={logo.alt}
-              className="logo-ticker-img"
-            />
-          ))}
-          {/* Second logo set for animation */}
-          {logos.map((logo) => (
-            <Image
-              key={logo.alt}
-              src={logo.src}
-              alt={logo.alt}
-              className="logo-ticker-img"
-            />
-          ))}
-        </motion.div>
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop",
+            }}
+          >
+            {/* First logo set for animation */}
+            {logos.map((logo) => (
+              <Image
+                key={logo.alt}
+                src={logo.src}
+                alt={logo.alt}
+                className="logo-ticker-img"
+              />
+            ))}
+            {/* Second logo set for animation */}
+            {logos.map((logo) => (
+              <Image
+                key={logo.alt}
+                src={logo.src}
+                alt={logo.alt}
+                className="logo-ticker-img"
+              />
+            ))}
+          </motion.div>
+        </div>
       </div>
     </div>
   );
