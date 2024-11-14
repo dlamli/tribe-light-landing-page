f8c309eff4217f923b768bb117245497cdb4e9bb
dlamli <dannylamli@hotmail.com>
2024-11-13 22:28:43 -0600

    feat:Add CTA Animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,11 +1,30 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
 import ArrowRight from "@/assets/arrow-right.svg";
 import StarImage from "@/assets/star.png";
 import SpringImage from "@/assets/spring.png";
-import Image from "next/image";
+import { useRef } from "react";
+import { useMotion } from "@/hooks";
 
 export const CallToAction = () => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const { translateY } = useMotion({
+    ref: sectionRef,
+    translateOption: {
+      inputRange: [0, 1],
+      outPutRange: [150, -150],
+    },
+  });
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title">Sign up for free today</h2>
@@ -13,15 +32,21 @@ export const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to trace
             your progress and motivates your efforts.
           </p>
-          <Image
-            src={StarImage}
+          <motion.img
+            src={StarImage.src}
             alt="Star Image"
             className="w-80 absolute -left-80 -top-32"
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            src={SpringImage}
+          <motion.img
+            src={SpringImage.src}
             alt="Spring Image"
             className="w-80 absolute -right-80 -top-4"
+            style={{
+              translateY,
+            }}
           />
         </div>
         <div className="flex items-center justify-center gap-2 mt-10">
